027a99fd238c0b1415ca3b3a47248c75f960e139
Casey Marshall <casey.s.marshall@gmail.com>
2016-11-16 14:15:05 -0800

    Support custom client SSL contexts.
    * src/aleph/netty.clj (check-ssl-args): new function.
      (ssl-client-context): add docstring, optional kwargs. Set up private key, certificate chain if requested.
    * src/aleph/tcp.clj (client): add ssl-context argument.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aleph "0.4.2-alpha8"
+(defproject io.bfpcorporation/aleph "0.4.2-alpha8"
   :description "a framework for asynchronous communication"
   :repositories {"jboss" "http://repository.jboss.org/nexus/content/groups/public/"
                  "sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
